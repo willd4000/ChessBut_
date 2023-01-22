6be0213bd11fdfcb7b3798f669a765fa26992361
willd <willd4000@gmail.com>
2023-01-22 17:29:39 -0600

    added chess board to scene

--- src/main/java/com/example/chessbut___/ChessApp.java
+++ src/main/java/com/example/chessbut___/ChessApp.java
@@ -11,7 +11,7 @@ public class ChessApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(ChessApp.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 800, 800);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
